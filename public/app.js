f8ff809e1565e2fc3df555b3bc035bc88ea267a6
Israel Harvin <izzy84.ih@gmail.com>
2019-08-29 11:04:34 -0400

    got scrap link to work

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 $.getJSON("/manga", function (data) {
     for (var i = 0; i < data.length; i++) {
-        $("#showdisplay").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "</p><img src='"+ data[i].imgSrc + "'>");
+        $("#showdisplay").append("<a href='" + data[i].link + "'><p data-id='" + data[i]._id + "'>" + data[i].title + "<img src='" + data[i].imgSrc + "'></p></a>");
     }
 });
 
